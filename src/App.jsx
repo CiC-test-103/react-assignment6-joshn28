7cdd8b46e3ffeb14030fd96d40b3e9894d214e6f
Joshua Nobleza <joshnobleza@bell.net>
2025-07-29 21:43:12 -0400

    Complete article preview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-/** Component Imports */
-import Example from './components/Example'
+import Card from "./components/Card";
 
 /**
  * This is the entry point for your react-app
@@ -9,27 +8,13 @@ import Example from './components/Example'
  *    - An example has been provided for you, see the 'Example' component
  *    - The 'Example.jsx' component comes with an 'Example.module.css'
  *    - Pay attention to variable file import (gives access to CSS variables)
- *    - Pay special attention to how we apply the className from the imported 'styles' 
+ *    - Pay special attention to how we apply the className from the imported 'styles'
  */
 
 const App = () => {
   return (
-    <div>
-      {<Example /> /** You can remove this when you wish, it's provided as an example of modular styling */}
-      
-      Shift the overall look and feel by adding these wonderful 
-      touches to furniture in your home
+    <Card />
+  );
+};
 
-      Ever been in a room and felt like something was missing? Perhaps 
-      it felt slightly bare and uninviting. I’ve got some simple tips 
-      to help you make any room feel complete.
-
-      Michelle Appleton
-      28 Jun 2020
-
-      Share
-    </div>
-  )
-}
-
-export default App
+export default App;
